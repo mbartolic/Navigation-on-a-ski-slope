c58acf102439117739916ecca1a586f0e5e0cf83
rskuljev <rskuljev@foi.hr>
2015-10-09 19:55:02 +0200

    Pocetna
    Prazna mapa spremna za rad

---
+++ app/src/androidTest/java/com/example/rene/navigation_on_a_ski_slope/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.rene.navigation_on_a_ski_slope;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
