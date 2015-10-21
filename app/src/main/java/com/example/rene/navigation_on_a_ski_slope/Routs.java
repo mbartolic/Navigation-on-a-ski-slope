e6017d01b59b2d2551744f42cf20069ffaf91ffe
tvunak <t.vunak@gmail.com>
2015-10-21 03:11:36 +0200

    Revert "Promjenjene sitnice kod staza"
    This reverts commit ce97dbf0997ddcc674531e284a987275b42ba1ec.

--- app/src/main/java/com/example/rene/navigation_on_a_ski_slope/Routs.java
+++ app/src/main/java/com/example/rene/navigation_on_a_ski_slope/Routs.java
@@ -20,13 +20,13 @@ public class Routs extends ActionBarActivity {
         Routes_object routs1 = new Routes_object("Sljeme",5100,1220,2600);
         Routes_object routs2 = new Routes_object("Ucka",250,250,255);
         Routes_object routs3 = new Routes_object("Garmisch-partenkirchen",250,250,255);
-        Routes_object routs4 = new Routes_object("Kranjska gora",300,250,855);
-        Routes_object routs5 = new Routes_object("Bormio",150,250,455);
-        Routes_object routs6 = new Routes_object("Flachau",150,250,455);
-        Routes_object routs7 = new Routes_object("Val-d'isere",150,250,455);
-        Routes_object routs8 = new Routes_object("Rogla",150,250,455);
-        Routes_object routs9 = new Routes_object("Savognin",150,250,455);
-        Routes_object routs10 = new Routes_object("Kitubuhel",150,250,455);
+        Routes_object routs4 = new Routes_object("Routes4",300,250,855);
+        Routes_object routs5 = new Routes_object("Routes5",150,250,455);
+        Routes_object routs6 = new Routes_object("Routes5",150,250,455);
+        Routes_object routs7 = new Routes_object("Routes5",150,250,455);
+        Routes_object routs8 = new Routes_object("Routes5",150,250,455);
+        Routes_object routs9 = new Routes_object("Routes5",150,250,455);
+        Routes_object routs10 = new Routes_object("Routes5",150,250,455);
 
         Routes_object[] routesObjects = {routs1, routs2, routs3, routs4, routs5, routs6, routs7, routs8, routs9, routs10};
         ListAdapter listAdapter = new Routes_adapter(this, routesObjects);
