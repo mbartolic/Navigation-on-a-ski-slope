d1ee85f19897505f4edfe037d96da9447ba5b7f8
rskuljev <rskuljev@foi.hr>
2016-01-26 23:20:56 +0100

    Algoritam
    -unesena je ruta po kojoj se krece korisnik
    -odredena je ruta
    -tocka skretanja

--- app/src/main/java/com/example/rene/navigation_on_a_ski_slope/AverageDirection.java
+++ app/src/main/java/com/example/rene/navigation_on_a_ski_slope/AverageDirection.java
@@ -1,5 +1,15 @@
 package com.example.rene.navigation_on_a_ski_slope;
 
+import android.graphics.Point;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * Created by Rene on 22.12.2015..
  */
@@ -13,46 +23,80 @@ public class AverageDirection {
     }
 
     private void AvgDirection(){  //algoritam u c# koji treba prevesti u javu a racuna trenutni smjer i predvida buduci smjer
-        /* int x1, y1, x2, y2, rez;
+        int x1, y1, x2, y2, rez;
         Point p, futurePoint;
-        bool zastavica1 = false, zastavica2 = false;
-        List<Point> point = new List<Point>();
-        private void AvgDirection()
-        {
-            int i = pointHistory.Count() - 1;
-            if (pointHistory.Count() > 3)
-            {
-                if ((pointHistory[i].X + pointHistory[i].Y) < (pointHistory[i - 1].X + pointHistory[i - 1].Y) && (pointHistory[i - 1].X + pointHistory[i - 1].Y) > (pointHistory[i - 2].X + pointHistory[i - 2].Y))
-                {
-                    x1 = pointHistory[i - 1].X;
-                    y1 = pointHistory[i - 1].Y;
-                    labCurrentDirection.Text = "Left";
-                    zastavica1 = true;
-                }
-                if ((pointHistory[i].X + pointHistory[i].Y) > (pointHistory[i - 1].X + pointHistory[i - 1].Y) && (pointHistory[i - 1].X + pointHistory[i - 1].Y) < (pointHistory[i - 2].X + pointHistory[i - 2].Y))
-                {
-                    x2 = pointHistory[i - 1].X;
-                    y2 = pointHistory[i - 1].Y;
-                    labCurrentDirection.Text = "Right";
-                    zastavica2 = true;
-                }
-                if (zastavica1 && zastavica2)
-                {
-                    p.X = (x1 + x2) / 2;
-                    p.Y = (y1 + y2) / 2;
-                    zastavica1 = false;
-                    zastavica2 = false;
-                    point.Add(p);
-                    int j = point.Count();
+        boolean zastavica1 = false;
+        boolean zastavica2 = false;
 
-                    if (j >= 2)
-                    {
-                        int z = point[j - 1].X - point[j - 2].X;
-                        int zz = point[j - 1].Y - point[j - 2].Y;
-                        futurePoint.X = point[j - 1].X + z;
-                        futurePoint.Y = point[j - 1].Y + zz;
-                    }
+
+
+    private void AvgDirection()
+    {
+        int i = pointHistory.Count() - 1;
+        if (pointHistory.Count() > 3)
+        {
+            if ((pointHistory[i].X + pointHistory[i].Y) < (pointHistory[i - 1].X + pointHistory[i - 1].Y) && (pointHistory[i - 1].X + pointHistory[i - 1].Y) > (pointHistory[i - 2].X + pointHistory[i - 2].Y))
+            {
+                x1 = pointHistory[i - 1].X;
+                y1 = pointHistory[i - 1].Y;
+                labCurrentDirection.Text = "Left";
+                zastavica1 = true;
+            }
+            if ((pointHistory[i].X + pointHistory[i].Y) > (pointHistory[i - 1].X + pointHistory[i - 1].Y) && (pointHistory[i - 1].X + pointHistory[i - 1].Y) < (pointHistory[i - 2].X + pointHistory[i - 2].Y))
+            {
+                x2 = pointHistory[i - 1].X;
+                y2 = pointHistory[i - 1].Y;
+                labCurrentDirection.Text = "Right";
+                zastavica2 = true;
+            }
+            if (zastavica1 && zastavica2)
+            {
+                p.X = (x1 + x2) / 2;
+                p.Y = (y1 + y2) / 2;
+                zastavica1 = false;
+                zastavica2 = false;
+                point.Add(p);
+                int j = point.Count();
+
+                if (j >= 2)
+                {
+                    Graphics g = this.CreateGraphics();
+                    float thickness = 2;
+                    g.DrawLine(new Pen(Brushes.Blue, thickness), point[j - 2], point[j - 1]);
+                    int z = point[j - 1].X - point[j - 2].X;
+                    int zz = point[j - 1].Y - point[j - 2].Y;
+                    futurePoint.X = point[j - 1].X + z;
+                    futurePoint.Y = point[j - 1].Y + zz;
+                    angleBetweenPoints(futurePoint);
+                    g.DrawLine(new Pen(Brushes.Yellow, thickness), point[j - 1], futurePoint);
                 }
-            }*/
+            }
+        }
+        else if(pointHistory.Count() <= 3 && pointHistory.Count() > 1) {//if( > 3)
+            Graphics g = this.CreateGraphics();
+            float thickness = 2;
+            g.DrawLine(new Pen(Brushes.Blue, thickness), pointHistory[1], pointHistory[0]);
+        }
+    }
+    Point VectorAfterTurn =  new Point();
+    Point VectorBeforeTurn = new Point();
+    private Double angleBetweenPoints(Point PointAtTheMoment) {
+
+        VectorBeforeTurn.X = PointAtTheMoment.X - turn.X;
+        VectorBeforeTurn.Y = PointAtTheMoment.Y - turn.Y;
+
+        VectorAfterTurn.X = path.X - turn.X;
+        VectorAfterTurn.Y = path.Y - turn.Y;
+
+        double VBTA = Math.Sqrt(Math.Pow(VectorBeforeTurn.X, 2) + Math.Pow(VectorBeforeTurn.Y, 2));
+        double VATA = Math.Sqrt(Math.Pow(VectorAfterTurn.X, 2) + Math.Pow(VectorAfterTurn.Y, 2));
+
+        double Product = VectorBeforeTurn.X * VectorAfterTurn.X + VectorBeforeTurn.Y * VectorAfterTurn.Y;
+
+        double Angle = (180 / Math.PI) * Math.Acos(Product / (VBTA * VATA));
+
+        Toast.makeText(getApplicationContext(), Angle + "", Toast.LENGTH_LONG).show();
+        return Angle;
+    }
     }
 }
--- app/src/main/java/com/example/rene/navigation_on_a_ski_slope/MyLocationGPS.java
+++ app/src/main/java/com/example/rene/navigation_on_a_ski_slope/MyLocationGPS.java
@@ -18,6 +18,12 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 
 public class MyLocationGPS extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
@@ -37,47 +43,87 @@ public class MyLocationGPS extends FragmentActivity implements OnMapReadyCallbac
             }
 
 
-
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;       //new object google map
         mMap.setMyLocationEnabled(true);    //enabeling current location (showning it on map)
         mMap.setOnMapLongClickListener(myOnMapLongClickListener);   //creating listener on map
-/*
-        PolylineOptions rectOptions =  new PolylineOptions()
-                .add(new LatLng(46.308408, 16.346646))
-                .add(new LatLng(46.308408, 16.346646))
-                .add(new LatLng(46.296015, 16.341840))
-                .width(8)
-                .color(Color.BLUE);
-
-        mMap.addPolyline(rectOptions);*/
     }
 
 
     GoogleMap.OnMapLongClickListener myOnMapLongClickListener = new GoogleMap.OnMapLongClickListener() {
         @Override
         public void onMapLongClick(LatLng latLng) {
-            loc.setLatitude(46.308436);
-            loc.setLongitude(16.330984);
-            Location myLocation = mMap.getMyLocation();
-            myl.setLatitude(myLocation.getLatitude());
-            myl.setLongitude(myLocation.getLongitude());
-            distanceFromPicture = new DistanceFromPicture();
-           Toast.makeText(getApplicationContext(), distanceFromPicture.getDistance(loc, myl) + "", Toast.LENGTH_LONG).show();
-
+            loc.setLatitude(48.221094);      //location in wien
+            loc.setLongitude(16.377882);     //location in wien
+          //  Location myLocation = mMap.getMyLocation();
+          //  myl.setLatitude(myLocation.getLatitude());
+          //  myl.setLongitude(myLocation.getLongitude());
+              distanceFromPicture = new DistanceFromPicture();
             float distance = distanceFromPicture.getDistance(loc,myl);
-            if(distance > 100){
-                Intent intent =  new Intent(context, DisplayImage.class);
+           Toast.makeText(getApplicationContext(), distance + "", Toast.LENGTH_LONG).show();
+
+            Location point1, point2, point3, point4, point5, point6, point7, point8, point9, point10, point11, point12;
+            point1 = new Location("point1");
+            point1.setLatitude(48.220277);
+            point1.setLongitude(16.378566);
+            point2 = new Location("point2");
+            point2.setLatitude(48.220377);      //64.599018
+            point2.setLongitude(16.378641);
+            point3 = new Location("point3"); //right point
+            point3.setLatitude(48.220559);   //64.599318   collect
+            point3.setLongitude(16.378759);  //
+            point4 = new Location("point4");
+            point4.setLatitude(48.220666);
+            point4.setLongitude(16.378518);     //64.599184
+            point5 = new Location("point5");
+            point5.setLatitude(48.220666);      //64.598910
+            point5.setLongitude(16.378244);
+            point6 = new Location("point6"); //left point
+            point6.setLatitude(48.220670);   //64.598722  collect
+            point6.setLongitude(16.378051);  //
+            point7 = new Location("point7");
+            point7.setLatitude(48.220770);      //64.598848
+            point7.setLongitude(16.378078);
+            point8 = new Location("point8");
+            point8.setLatitude(48.220924);      //64.599061
+            point8.setLongitude(16.378137);
+            point9 = new Location("point9");  //right point collect
+            point9.setLatitude(48.220970);    //64.599122
+            point9.setLongitude(16.378152);   //
+            point10 = new Location("point10");
+            point10.setLatitude(48.220999);     //64.598926
+            point10.setLongitude(16.377927);
+            point11 = new Location("point11");
+            point11.setLatitude(48.221035);   //64.598989  collect
+            point11.setLongitude(16.377954);
+            point12 = new Location("point12");
+            point12.setLatitude(48.221060); //64.598934
+            point12.setLongitude(16.377874);
+            List<Location> myLocations = new ArrayList<Location>();
+            myLocations.add(point1);
+            myLocations.add(point2);
+            myLocations.add(point3);
+            myLocations.add(point4);
+            myLocations.add(point5);
+            myLocations.add(point6);
+            myLocations.add(point7);
+            myLocations.add(point8);
+            myLocations.add(point9);
+            myLocations.add(point10);
+            myLocations.add(point11);
+            myLocations.add(point12);
+
+
+
+            if(distance > 100) {
+                Intent intent = new Intent(context, DisplayImage.class);
                 startActivity(intent);
+                ConvertingGpsCoordToXY convertingGpsCoordToXY = new ConvertingGpsCoordToXY();  //slanje gps podataka za konvertiranje
+               // double convX = convertingGpsCoordToXY.convertLat(myLocation.getLatitude());
+               // double convY = convertingGpsCoordToXY.convertLon(myLocation.getLongitude());
+               // AverageDirection averageDirection = new AverageDirection(convX, convY);
             }
-
-            ConvertingGpsCoordToXY convertingGpsCoordToXY =  new ConvertingGpsCoordToXY();  //slanje gps podataka za konvertiranje
-            double convX = convertingGpsCoordToXY.convertLat(myLocation.getLatitude());
-            double convY = convertingGpsCoordToXY.convertLon(myLocation.getLongitude());
-            AverageDirection averageDirection = new AverageDirection(convX, convY);
-
           /*  CircleOptions circleOptions = new CircleOptions()
                     .center(new LatLng(46.308408, 16.346646))
                     .radius(50);
