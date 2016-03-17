5ae196503628e8f9dc34e39b3abd6a03d4992c50
rskuljev <rskuljev@foi.hr>
2016-03-17 23:32:39 +0100

    Zavrsne promjene
    -uređivanje klasa i koda
    -sreden algoritam i sitni popravci napravljeni

--- app/src/main/java/com/example/rene/navigation_on_a_ski_slope/MainActivity.java
+++ app/src/main/java/com/example/rene/navigation_on_a_ski_slope/MainActivity.java
@@ -3,12 +3,10 @@ package com.example.rene.navigation_on_a_ski_slope;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/example/rene/navigation_on_a_ski_slope/MyLocationGPS.java
+++ app/src/main/java/com/example/rene/navigation_on_a_ski_slope/MyLocationGPS.java
@@ -3,6 +3,7 @@ package com.example.rene.navigation_on_a_ski_slope;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -20,7 +21,6 @@ import java.util.List;
 
 public class MyLocationGPS extends Activity implements LocationListener, CoordiantesView{
 
-    //Objekt klase koja implementira sucelje CoordinatesPresenter
     private LocationManager mLocMgr;
     CoordinatesPresenter coordinatesPresenter;
     private static final String LOG_KEY = "coordianes";
@@ -31,44 +31,32 @@ public class MyLocationGPS extends Activity implements LocationListener, Coordia
     MyPointDouble locTurn =  new MyPointDouble();
     MyPointDouble locAfterTurn = new MyPointDouble();
     String turnLR;
-    LocationManager lm;
-    TextView rene;
 
+    List<Location> myLocLeftSlopeTrack;
+    List<Location> myLocLeftSlopeSkii;
 
     Location myLocation ;
     List<MyPointDouble> myLocHist;
     MyPointDouble point;
 
     DistanceFromPoint distanceFromPoint;
-  //  Location loc =  new Location("Loc");
-  //  Location myl = new Location("Myloc");
       final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_con);
-        rene = (TextView) findViewById(R.id.rene);
-        myLocations = new ArrayList<MyPointDouble>();
+        myLocations = new ArrayList<>();
+        myLocLeftSlopeSkii = new ArrayList<>();
+
         mLocMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
-        mLocMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                6000, 0, this);
+        mLocMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000, 0, this);
+
         //Inicijalizacija klase koja implementira sucelje CoordinatesPresenter
         coordinatesPresenter = new CoordinatesPresenterImpl(this);
         //Dohvacanje koordinata preko sucelja CoordinatesPresenter
         coordinatesPresenter.getData(Constants.SOURCE_POINTS, Constants.DESTINATION_POINTS, Constants.CAR_ROUTE_TYPE, Constants.VOICE_INSTRUCTIONS, Constants.LANGUAGE);
-        /*lm   = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        lm = (LocationManager)this.getSystemService(LOCATION_SERVICE);
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER); //<5>*/
-        //mLocMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
-        //mLocMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-            //    500, 0, this);
-        //if (location != null) {
-
-          //  this.onLocationChanged(location); //<6>
-        //}
-        //lm.requestLocationUpdates("cxy",1000,1,locationListener);
         if (savedInstanceState == null) {
             Bundle bundle = getIntent().getExtras();
             if(bundle == null) {
@@ -117,28 +105,28 @@ public class MyLocationGPS extends Activity implements LocationListener, Coordia
         }else if (track == 2){
             MyTrackPoints point1, point2, point3, point4, point5, point6;
             point1 = new MyTrackPoints();
-            point1.y =  46.306511;
-            point1.x =  16.344795;
+            point1.y =  46.307745;
+            point1.x =  16.338490;
             point1.turn = 0;
             point2 = new MyTrackPoints();
-            point2.y = 46.306828;
-            point2.x = 16.344650;
+            point2.y = 46.307744;
+            point2.x = 16.338404;
             point2.turn = 0;
             point3 = new MyTrackPoints();
-            point3.y = 46.307087;
-            point3.x = 16.344553;
-            point3.turn = 1;                //turning point
+            point3.y = 46.307740;
+            point3.x = 16.338262;
+            point3.turn = 0;
             point4 = new MyTrackPoints();
-            point4.y = 46.307154;
-            point4.x = 16.344864;
+            point4.y = 46.307742;
+            point4.x = 16.338164;
             point4.turn = 0;
             point5 = new MyTrackPoints();
-            point5.y =  46.307213;
-            point5.x =  16.345132;
-            point5.turn = 0;
+            point5.y =  46.307745;
+            point5.x =  16.338051;
+            point5.turn = 1;            //turning point
             point6 = new MyTrackPoints();
-            point6.y =  46.307289;
-            point6.x =  16.345642;
+            point6.y =  46.307802;
+            point6.x =  16.338051;
             point6.turn = 0;
 
             myTrackPointsList = new ArrayList<>();
@@ -240,154 +228,6 @@ public class MyLocationGPS extends Activity implements LocationListener, Coordia
                     Toast.LENGTH_SHORT).show();
         }
     }*/
-    /**
-     * Shows the angle on long click.
-     */
-    @Override
-    public void onStart(){
-        super.onStart();
-//------------------------------- Rene algoritam pocetak----------------------------------------------------------------------------------------//
-/*
-        MyPointDouble point1, point2, point3, point4, point5, point6, point7, point8, point9, point10, point11, point12, point13, point14, point15, point16, point17;
-        point1 = new MyPointDouble();
-        point1.y =  46.302580;
-        point1.x =  16.337077;
-        point2 = new MyPointDouble();
-        point2.y = 46.302571;
-        point2.x = 16.337106;
-        point3 = new MyPointDouble();
-        point3.y = 46.302545;
-        point3.x = 16.337145;
-        point4 = new MyPointDouble();
-        point4.y = 46.302511;
-        point4.x = 16.337123;
-        point5 = new MyPointDouble();
-        point5.y =  46.302473;
-        point5.x =  16.337096;        //turning point
-        point6 = new MyPointDouble();
-        point6.y =  46.302427;
-        point6.x =  16.337153;
-        point7 = new MyPointDouble();
-        point7.y =  46.302403;
-        point7.x =  16.337206;
-        point8 = new MyPointDouble();
-        point8.y = 46.302351;
-        point8.x = 16.337229;
-        point9 = new MyPointDouble();
-        point9.y = 46.302276;
-        point9.x = 16.337227;
-        point10 = new MyPointDouble();
-        point10.y = 46.302192;
-        point10.x = 16.337217;
-        point11 = new MyPointDouble();
-        point11.y =  46.302129;
-        point11.x =  16.337271;        //turning point
-        point12 = new MyPointDouble();
-        point12.y =  46.302081;
-        point12.x =  16.337337;
-        point13 = new MyPointDouble();
-        point13.y = 46.302017;
-        point13.x = 16.337333;
-        point14 = new MyPointDouble();
-        point14.y =  46.301974;
-        point14.x =  16.337286;        //turning point
-        point15 = new MyPointDouble();
-        point15.y =  46.301908;
-        point15.x =  16.337359;
-        point16 = new MyPointDouble();
-        point16.y = 46.301860;
-        point16.x = 16.337426;
-        point17 = new MyPointDouble();
-        point17.y =  46.301816;
-        point17.x =  16.337391;
-
-        myLocations = new ArrayList<>();
-        myLocations.add(point1);
-        myLocations.add(point2);
-        myLocations.add(point3);
-        myLocations.add(point4);
-        myLocations.add(point5);
-        myLocations.add(point6);
-        myLocations.add(point7);
-        myLocations.add(point8);
-        myLocations.add(point9);
-        myLocations.add(point10);
-        myLocations.add(point11);
-        myLocations.add(point12);
-        myLocations.add(point13);
-        myLocations.add(point14);
-        myLocations.add(point15);
-        myLocations.add(point16);
-        myLocations.add(point17);
-         //   Location myLocation = mMap.getMyLocation();
-          //  myl.setLatitude(myLocation.getLatitude());
-          //  myl.setLongitude(myLocation.getLongitude());
-          //  distanceFromPoint = new DistanceFromPoint();
-         //   float distance = distanceFromPoint.getDistance(loc,myl);
-           // Toast.makeText(getApplicationContext(), distance + "", Toast.LENGTH_LONG).show();
-            ConvertingGpsCoordToXY convertingGpsCoordToXY = new ConvertingGpsCoordToXY();
-            List<MyPointDouble> myLocHist;
-            myLocHist = new ArrayList<>();
-
-            for(int i = 0; i< myLocations.size(); i++) {
-                MyPointDouble converted = new MyPointDouble();
-                converted.y = convertingGpsCoordToXY.convertLon(myLocations.get(i).y);
-                converted.x = convertingGpsCoordToXY.convertLat(myLocations.get(i).x);
-                myLocHist.add(converted);
-            }
-           // if(distance > 100) {
-              //  double convX = convertingGpsCoordToXY.convertLat(myLocation.getLatitude());
-              //  double convY = convertingGpsCoordToXY.convertLon(myLocation.getLongitude());
-
-        AverageDirection averageDirection = new AverageDirection();
-        double angle = averageDirection.AvgDirection(myLocHist, turnPoint);
-        Toast.makeText(getApplicationContext(), angle + "", Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(context, DisplayImage.class);
-        intent.putExtra("EXTRA_ANGLE", angle);
-        intent.putExtra("turnLR", turnLR);
-        startActivity(intent);
-*/
-
-        //-------------------------------- Rene algoritam kraj------------------------------------------------------------------------------//
-        //------------------------------ Vunak Vunce algoritam pocetak-----------------------------------------------------------------------------//
-    /*    Location skiLocation, skiLocation1, skiLocation2, skiLocation3, skiLocation4;
-        skiLocation = new Location("skiLocation");
-        skiLocation.setLatitude(48.221194);      //skier location in Wien, for dynamic use we should use current location of skier
-        skiLocation.setLongitude(16.377282);
-        skiLocation1 = new Location("skiLocation1");
-        skiLocation1.setLatitude(48.220822);
-        skiLocation1.setLongitude(16.381717);
-        skiLocation2 = new Location("skiLocation2");
-        skiLocation2.setLatitude(48.221794);
-        skiLocation2.setLongitude(16.382114);
-        skiLocation3 = new Location("skiLocation3");
-        //      skiLocation3.setLatitude(48.221194);
-        //      skiLocation3.setLongitude(16.377282);
-        skiLocation3.setLatitude(48.222537);
-        skiLocation3.setLongitude(16.382457);
-        skiLocation4 = new Location("skiLocation4");
-        skiLocation4.setLatitude(48.223602);
-        skiLocation4.setLongitude(16.382918);
-
-        List<Location> skiLocations = new ArrayList<>();
-        skiLocations.add(skiLocation);
-        skiLocations.add(skiLocation1);
-        skiLocations.add(skiLocation2);
-        skiLocations.add(skiLocation3);
-        skiLocations.add(skiLocation4);
-
-
-        UserLocationStatus userLocationStatus = new UserLocationStatus();
-        String mess = userLocationStatus.CalculatingIfUserLeftSlope(trackLocations, skiLocations);
-
-        Toast.makeText(getApplicationContext(), mess + "", Toast.LENGTH_LONG).show();*/
-
-        //------------------------------------- Vunak Vunce algoritam kraj -----------------------------------------------------------------------------//
-
-            }
-
-
-
 
     @Override
     public void onBackPressed() {
@@ -395,6 +235,8 @@ public class MyLocationGPS extends Activity implements LocationListener, Coordia
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        if(mLocMgr !=null)
+        mLocMgr.removeUpdates(this);
         finish();
 
 
@@ -403,24 +245,17 @@ public class MyLocationGPS extends Activity implements LocationListener, Coordia
 @Override
     protected void onStop(){
         super.onStop();
+    if(mLocMgr !=null)
+    mLocMgr.removeUpdates(this);
     finish();
     }
 
-    private LocationManager locationManager;
-    private LocationListener locationListener;
-
-
     @Override
     public void onLocationChanged(Location location) {
-        rene.setText("radi");
-        Toast.makeText(getApplicationContext(),"YUPIIIIIIIIIIII", Toast.LENGTH_LONG).show();
-
         ConvertingGpsCoordToXY convertingGpsCoordToXY = new ConvertingGpsCoordToXY();
 
         myLocation = new Location(location);
         myLocHist = new ArrayList<>();
-        //myLocation.setLatitude(location.getLatitude());
-        //myLocation.setLongitude(location.getLongitude());
         distanceFromPoint = new DistanceFromPoint();
         Location l1 = new Location(LocationManager.GPS_PROVIDER);
 
@@ -429,11 +264,11 @@ public class MyLocationGPS extends Activity implements LocationListener, Coordia
         float distance = distanceFromPoint.getDistance(l1,myLocation);
 
         point = new MyPointDouble();
-        point.y =  location.getLongitude();
-        point.x =  location.getLatitude();
+        point.x =  location.getLongitude();
+        point.y =  location.getLatitude();
         myLocations.add(point);
 
-        Toast.makeText(getApplicationContext(),point.y + ", " + point.x + ", udaljenost: " + distance + ".", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"Distance from turn: " + distance + " m", Toast.LENGTH_SHORT).show();
 
 
          if(distance < 10) {
@@ -445,29 +280,40 @@ public class MyLocationGPS extends Activity implements LocationListener, Coordia
              }
              AverageDirection averageDirection = new AverageDirection();
              double angle = averageDirection.AvgDirection(myLocHist, turnPoint);
-             Toast.makeText(getApplicationContext(), angle + "", Toast.LENGTH_LONG).show();
+             Toast.makeText(getApplicationContext(), angle + "", Toast.LENGTH_SHORT).show();
              Intent intent = new Intent(context, DisplayImage.class);
              intent.putExtra("EXTRA_ANGLE", angle);
              intent.putExtra("turnLR", turnLR);
              startActivity(intent);
          }
 
+        myLocLeftSlopeTrack =  new ArrayList<>();
+        myLocLeftSlopeSkii.add(location);
+        for(int i = 0; i < myTrackPointsList.size(); i++){
+            Location loc = new Location(location);
+            loc.setLongitude(myTrackPointsList.get(i).y);
+            loc.setLatitude(myTrackPointsList.get(i).x);
+            myLocLeftSlopeTrack.add(loc);
+        }
+        UserLocationStatus userLocationStatus = new UserLocationStatus();
+        String mess = userLocationStatus.CalculatingIfUserLeftSlope(myLocLeftSlopeTrack, myLocLeftSlopeSkii);
+
+        TextView textView;
+        textView = (TextView) findViewById(R.id.editText);
+        if(mess != null) {
+            textView.setText(mess);
+            textView.setTextColor(Color.RED);
+        }
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
 
     @Override
-    public void onProviderEnabled(String provider) {
-
-    }
+    public void onProviderEnabled(String provider) {}
 
     @Override
-    public void onProviderDisabled(String provider) {
+    public void onProviderDisabled(String provider) {}
+
 
     }
-    //  }
-
-    };
