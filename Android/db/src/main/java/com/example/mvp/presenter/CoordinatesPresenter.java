cf5f252a33cc3c2add1a8b2c336afed98c0f45c5
mbartolic <mbartoli@foi.hr>
2016-06-23 02:19:10 +0200

    Ispravak pogresnog commita

--- Android/db/src/main/java/com/example/mvp/presenter/CoordinatesPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.example.mvp.presenter;
-
-/**
- * Interface for fetching coordinates.
- * Created by Rene on 6.12.2015..
- */
-public interface CoordinatesPresenter {
-    void getData(String sourcePoint, String destionationPoint, String routeType, String voiceInstructions, String language);
-}
